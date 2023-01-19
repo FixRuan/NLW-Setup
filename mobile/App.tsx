240139fef0434ff6f62fa15f24fa9c7739c6e726
fixruan <ruangoio01@gmail.com>
2023-01-19 14:09:35 -0300

    chore: fonts and loading component config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+import { Loading } from "./src/components/Loading";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from "@expo-google-fonts/inter";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{ color: "red" }}>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar translucent={false} backgroundColor="#09090A" />
+
+      <View style={styles.container}>
+        <Text style={{ color: "#7C3AED", fontWeight: "bold", fontSize: 32 }}>
+          Hello World
+        </Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#dddddd",
+    backgroundColor: "#09090A",
     alignItems: "center",
     justifyContent: "center",
   },
