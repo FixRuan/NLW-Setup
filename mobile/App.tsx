26a1272473d8dce050341dd507d8f1fc6822d18a
fixruan <ruangoio01@gmail.com>
2023-01-19 13:41:44 -0300

    chore: import assets

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: "red" }}>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
@@ -14,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#dddddd",
     alignItems: "center",
     justifyContent: "center",
   },
