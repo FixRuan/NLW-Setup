d4bf98ec3413d3b5540368b3a7b65dd6b77308f0
fixruan <ruangoio01@gmail.com>
2023-01-20 12:06:46 -0300

    chore: prisma tables config

---
+++ server/prisma/migrations/20230120145324_create_rest_of_the_tables/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "HabitWeekDays" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "habit_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Day" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "day_habits" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Day_date_key" ON "Day"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_habits_day_id_habit_id_key" ON "day_habits"("day_id", "habit_id");
