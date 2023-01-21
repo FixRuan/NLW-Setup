3fc8ef81e2b76226c0f474a22b9b64cdd1fa47d4
fixruan <ruangoio01@gmail.com>
2023-01-20 23:43:48 -0300

    feat: add get completed habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -36,7 +36,9 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     const { date } = getDayParams.parse(request.query);
-    const weekDay = dayjs(date).get("day");
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -51,7 +53,20 @@ export async function appRoutes(app: FastifyInstance) {
       }
     });
 
-    return { possibleHabits };
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
+    return { possibleHabits, completedHabits };
   });
 }
 
