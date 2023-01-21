ebf840322902185d430d4a86a96a4faab89776d0
fixruan <ruangoio01@gmail.com>
2023-01-21 20:27:02 -0300

    feat: generate future days

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,8 @@ import { HabitDay } from "./HabitDay";
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 const summaryDates = generateDatesFromYearBegining();
+const minimumSummaryDatesSize = 18 * 7; //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
@@ -20,7 +22,13 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map(date => <HabitDay key={date.toString()} />)}
+        {summaryDates.map(date =>
+          <HabitDay key={date.toString()} />
+        )}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) =>
+          <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+        )}
       </div>
     </div>
   );
