9856ad572876bd660a03fe80e91c61247e7e4ae4
fixruan <ruangoio01@gmail.com>
2023-01-22 11:56:18 -0300

    feat: generate days to fill screen

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 import { generateDatesFromYearBegining } from "../utils/generate-dates-from-year-begining";
 const datesFromYearStart = generateDatesFromYearBegining();
+const minimumSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 export function Home() {
@@ -24,13 +27,29 @@ export function Home() {
         ))}
       </View>
 
-      <View className="flex-row flex-wrap">
-        {datesFromYearStart.map(date => (
-          <HabitDay
-            key={date.toISOString()}
-          />
-        ))}
-      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map(date => (
+            <HabitDay
+              key={date.toISOString()}
+            />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill })
+              .map((_, index) => (
+                <View
+                  key={index}
+                  className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                  style={{ width: DAY_SIZE, height: DAY_SIZE }}
+                />
+              ))
+          }
+        </View>
+      </ScrollView>
     </View>
   );
 }
