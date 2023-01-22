65b969b5d0fa928e17e6ad53283a87d7cda0e01b
fixruan <ruangoio01@gmail.com>
2023-01-22 12:24:07 -0300

    feat: toggle habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
 
 import dayjs from "dayjs";
-import { z } from "zod";
+import { date, z } from "zod";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/habits", async (request) => {
@@ -67,6 +67,54 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     return { possibleHabits, completedHabits };
+  });
+
+  app.patch("/habits/:id/toggle", async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = toggleHabitParams.parse(request.params);
+
+    const today = dayjs().startOf("day").toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      });
+    }
   });
 }
 
